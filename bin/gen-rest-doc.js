a259971aaa41f0a1b5fd12eab502258ef8914c17
Steve Krenek <steve.krenek@zyquest.com>
2012-09-27 13:37:43 -0500

    Generation is much more robust now.  Added conslidate options, and destFile can now be a directory or a file name.  Input can be a dir, js file, or json schema file.  The file is also now a proper CLI.

--- bin/gen-rest-doc.js
+++ bin/gen-rest-doc.js
@@ -1,48 +1,88 @@
 #!/usr/bin/env node
 
-var docGen = require("../index.js"),
-    fs = require('fs');
+var fs = require("fs"),
+    path = require("path"),
+    //color = require("colorize"),
+    cli = require("cli").enable('status'),
+    _ = require("underscore")._,
+    util = require("util"),
+    schemaGen = require("feather-rest-descriptor"),
+    docGen = require("../index.js");
 
-var readline = function (encoding, callback) {
-    if (typeof encoding === 'function') {
-        callback = encoding;
-        encoding = 'utf8';
-    }
-    var stream = process.openStdin(), 
-      data = '';
-    stream.setEncoding(encoding);
-    var listener = function (chunk) {
-        data += chunk;
-        if (data.indexOf('\n') >= 0) {
-          var endIndex = data.indexOf('\n');
-          var output = data.substr(0, endIndex).replace(/(\r)+$/, '');
-          // Cleanup the stream.
-          stream.removeListener('data', listener);
-          stream.pause();
-          // Return the output.
-          callback(output);
-        }
-    };
-    stream.on('data', listener);
+var cliOptions = {
+  input: ['i', 'Input path.  REQUIRED.  Can be a directory, .js file, or .json file.', 'path', null],
+  output: ['o', 'Output path.  If omitted, output will be sent to stdout.', 'path', null],
+  consolidate: ['c', 'Consolidate output into one HTML document.  This only makes sense if input is a directory.', 'boolean', false]
 };
 
+cli.parse(cliOptions);
 
-if (process.argv.length <= 2) {
-  console.log('Usage: gen-rest-doc /input/path.json /output/path.html');
-  process.exit();
-}
-
-var inFile = process.argv[2];
-var outFile = process.argv[3] || 'out.html';
-var data = JSON.parse(fs.readFileSync(inFile, 'utf8'));
-
-docGen.generateHtml({ schema: data, destFile: outFile }, function(errs) {
-  if (errs) {
-    var i = 0;
-    for (i = 0; i < errs.length; i++) {
-      console.error(errs[i]);
+var handleDirectoryInput = function(dir, interface, outputLocation, consolidate) {
+  schemaGen.generateSchemas({
+    restFolder: dir,
+    interface: interface
+  }, function(err, schemas) {
+    if (err) {
+      cli.error(err);
+    } else {
+      docGen.generateHtml({
+        schemas: schemas,
+        destFile: outputLocation,
+        consolidate: consolidate
+      }, function(err, document) {
+        if (err) {
+          cli.error(err);
+        } else if (document) {
+          console.log(document);
+        }
+        cli.ok("Generation complete.");
+      });
     }
-  } else {
-    console.log('Generation complete!');
-  }
+  });
+};
+
+var handleSchemaInput = function(file, outputLocation) {
+  var data = JSON.parse(fs.readFileSync(file, 'utf8'));
+  docGen.generateHtml({
+    schema: data,
+    destFile: outputLocation
+  }, function(err, document) {
+    if (err) {
+      cli.error(err);
+    } else if (document) {
+      console.log(document);
+    }
+    cli.ok("Generation complete.");
+  });
+};
+
+cli.main(function(args, options) {
+  cli.debug("Parsed options are: " + util.inspect(options));
+
+    if (!fs.existsSync(options.input)) {
+      cli.error("Input path '" + options.input + "' does not exist");
+      cli.getUsage();
+    } else {
+
+      // check out the input path.  If it is a json file, treat it as a schema.  
+      // If it is a js file, treat it as a REST file.  If it is a folder, treat it 
+      // as a folder of REST files.
+
+      var stat = fs.statSync(options.input);
+      
+      if (stat.isDirectory()) {
+        cli.ok("Generating documents for all interfaces in " + options.input);
+        handleDirectoryInput(options.input, 'all', options.output, options.consolidate);
+
+      } else if (path.extname(options.input) === '.js') {
+        cli.ok("Generating documents for " + options.input);
+        handleDirectoryInput(options.input, path.basename(options.input), options.output, false);
+
+      } else if (path.extname(options.input) === '.json') {
+        handleSchemaInput(options.input, options.output);
+
+      } else {
+        cli.error("Unrecognized input format.  Expecting a directory, .js, or .json file.");
+      }
+    }
 });
